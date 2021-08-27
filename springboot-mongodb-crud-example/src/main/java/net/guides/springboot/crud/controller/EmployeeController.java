e1c53f89d12173993c5d381602905f7df6f9769d
Davidpavan <David@zelarsoft.com>
2021-08-27 14:38:41 +0530

    First commit

--- springboot-mongodb-crud-example/src/main/java/net/guides/springboot/crud/controller/EmployeeController.java
+++ springboot-mongodb-crud-example/src/main/java/net/guides/springboot/crud/controller/EmployeeController.java
@@ -23,7 +23,9 @@ import net.guides.springboot.crud.model.Employee;
 import net.guides.springboot.crud.repository.EmployeeRepository;
 import net.guides.springboot.crud.service.SequenceGeneratorService;
 
-
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1")
 public class EmployeeController {
 	@Autowired
 	private EmployeeRepository employeeRepository;
