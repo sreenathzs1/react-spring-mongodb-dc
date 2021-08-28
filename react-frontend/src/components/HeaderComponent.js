c144ef4194d3671abf94e816b0245add5349d76c
Davidpavan <David@zelarsoft.com>
2021-08-28 12:49:46 +0530

    First commit

--- react-frontend/src/components/HeaderComponent.js
+++ react-frontend/src/components/HeaderComponent.js
@@ -14,7 +14,7 @@ class HeaderComponent extends Component {
             <div>
                 <header>
                     <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-                    <div><a  className="navbar-brand">Employee Management App</a></div>
+                    <div> className="navbar-brand">Employee Management App</div>
                     </nav>
                 </header>
             </div>
--- springboot-mongodb-crud-example/src/main/java/net/guides/springboot/crud/controller/EmployeeController.java
+++ springboot-mongodb-crud-example/src/main/java/net/guides/springboot/crud/controller/EmployeeController.java
@@ -23,7 +23,7 @@ import net.guides.springboot.crud.model.Employee;
 import net.guides.springboot.crud.repository.EmployeeRepository;
 import net.guides.springboot.crud.service.SequenceGeneratorService;
 
-@CrossOrigin(origins = "http://3.93.20.250:8080")
+@CrossOrigin(origins = "http://3.93.20.250:8081")
 @RestController
 @RequestMapping("/api/v1")
 public class EmployeeController {
