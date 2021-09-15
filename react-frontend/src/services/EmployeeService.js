7ae6d921d07a6254f5fecd172f9acb0568e826e1
Davidpavan <David@zelarsoft.com>
2021-09-15 16:14:03 +0530

    First commit

--- react-frontend/src/services/EmployeeService.js
+++ react-frontend/src/services/EmployeeService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const EMPLOYEE_API_BASE_URL = "http://localhost:8080/api/v1/employees";
+const EMPLOYEE_API_BASE_URL = "http://54.209.64.242:8080/api/v1/employees";
 
 class EmployeeService {
 
