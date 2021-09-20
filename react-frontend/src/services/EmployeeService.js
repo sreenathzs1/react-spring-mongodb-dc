0224cafab2a3a4be2eedca497fceaf84020bb387
Lakshmoji Yalamati <lakshmoji+sre@trysapling.com>
2021-09-20 17:26:18 +0530

    docker env

--- react-frontend/src/services/EmployeeService.js
+++ react-frontend/src/services/EmployeeService.js
@@ -1,27 +1,28 @@
 import axios from 'axios';
 
-const url = { url: process.env.REACT_APP_API_URL}; //backend service name
+const url = process.env.REACT_APP_API_URL; //backend service name
+
 
 class EmployeeService {
 
     getEmployees(){
-        return axios.get(process.env.REACT_APP_API_URL);
+        return axios.get(url);
     }
 
     createEmployee(employee){
-        return axios.post(process.env.REACT_APP_API_URL, employee);
+        return axios.post(url, employee);
     }
 
     getEmployeeById(employeeId){
-        return axios.get(process.env.REACT_APP_API_URL + '/' + employeeId);
+        return axios.get(url + '/' + employeeId);
     }
 
     updateEmployee(employee, employeeId){
-        return axios.put(process.env.REACT_APP_API_URL + '/' + employeeId, employee);
+        return axios.put(url+ '/' + employeeId, employee);
     }
 
     deleteEmployee(employeeId){
-        return axios.delete(process.env.REACT_APP_API_URL + '/' + employeeId);
+        return axios.delete(url + '/' + employeeId);
     }
 }
 
